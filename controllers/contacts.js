669b8f3649bf3a9a39497c9cee72be73242bfd1a
ryanwalk01 <ryan.walk01@gmail.com>
2025-01-23 11:02:20 -0700

    Corrected delete status code

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -35,7 +35,7 @@ const deleteContact = async (req, res, next) => {
   const contactId = new ObjectId(req.params.id);
   const result = await mongodb.getDb().db().collection('contacts').deleteOne({ _id: contactId });
   res.setHeader('Content-Type', 'application/json');
-  res.status(204).json(result);
+  res.status(200).json(result);
 };
 
 module.exports = { allContacts, getContactById, createContact, updateContact, deleteContact };
