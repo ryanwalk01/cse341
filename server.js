c36936c93738ff11d9dac34b39bb5a0afc5e8e2f
ryanwalk01 <ryan.walk01@gmail.com>
2025-01-30 12:28:30 -0700

    Fixed cors

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
   res.setHeader(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
-  )
+  );
   res.setHeader('Content-Type', 'application/json');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   next();
